147eb9ce5f16b5c49545ef2ad264bd3af82dd624
venetalobutova <73944898+venetalobutova@users.noreply.github.com>
2022-08-29 19:21:21 +0300

    Summator - Add files in a folder

---
+++ Summator/Summator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Summator
+
+{
+    public static class Summator
+
+    {
+        public static long Sum(int[] Array)
+        {
+            long sum = 0;
+
+            for (long i = 0; i < Array.Length; i++)
+            {
+                sum += Array[i];
+
+            }
+            return sum;
+
+        }
+        public static int Average(int[] Array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                sum += Array[i];
+
+            }
+            return sum / Array.Length;
+        }
+    }
+}
